2cde3a4b11f91da853ab56312c4b3dbc782f9fb0
surajbridgelabz <surajnsinha@gmail.com>
2022-11-08 16:24:52 +0530

    [Suraj] Add: create employee Table

--- Employee_SQL.sql
+++ Employee_SQL.sql
@@ -1,2 +1,11 @@
 ----------------UC1------ 
 create database Employee_Payroll
+
+--------UC2----------
+create table Employee
+(
+Id int NOT NULL PRIMARY KEY,
+ NAME VARCHAR(100) NOT NULL,
+ Salary FLOAT NOT NULL,
+ StartDate DATE NOT NULL,
+ )
